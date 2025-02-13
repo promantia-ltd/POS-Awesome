e2b4be024da7e2fa5dc3e76add3c8cdb0243a0fb
L-Satishkumar <satishkumar.l@promantia.com>
2025-02-13 15:08:27 +0530

    24445: customized the changes as mentioned in requirement. added item total field line wise and it will editable to only when allow user to edit item total is checked.

--- posawesome/hooks.py
+++ posawesome/hooks.py
@@ -257,6 +257,7 @@ fixtures = [
                     "POS Profile-posa_allow_reconcile_payments",
                     "POS Profile-column_break_uolvm",
                     "POS Profile-posa_allow_mpesa_reconcile_payments",
+                    "POS Profile-custom_allow_user_to_edit_item_total"
                 ),
             ]
         ],
