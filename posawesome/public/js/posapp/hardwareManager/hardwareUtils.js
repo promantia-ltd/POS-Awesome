67dd3237f21a12eec39a7d4617b161127f56e633
Siddarthmalemath <siddarth.m@promantia.com>
2025-06-26 17:33:16 +0530

    [26011] Code optimised.

--- posawesome/public/js/posapp/hardwareManager/hardwareUtils.js
+++ posawesome/public/js/posapp/hardwareManager/hardwareUtils.js
@@ -1,93 +1,65 @@
+const frappeCall = (method, args = {}) =>
+  frappe.call({ method, args }).then((r) => {
+    if ("message" in r) return r.message;
+    throw new Error("No message returned");
+  });
+
 export default {
-  data() {},
   methods: {
     hardwareConfiguration(pos_name) {
-      return new Promise((resolve, reject) => {
-        frappe.call({
-          method:
-            "awesome_pos_hw.api.get_hardware_details.get_hardware_manager_setting",
-          args: { pos_profile_name: pos_name },
-          callback: function (r) {
-            console.log(r);
-
-            if ("message" in r) {
-              resolve(r.message); // allow true, false, 0, etc.
-            } else {
-              reject("No message returned");
-            }
-          },
-          error: function (err) {
-            reject(err);
-          },
-        });
-      });
+      return frappeCall(
+        "awesome_pos_hw.api.get_hardware_details.get_hardware_manager_setting",
+        { pos_profile_name: pos_name }
+      );
     },
-    hardwareURL(api_name) {
-      return new Promise((resolve, reject) => {
-        frappe.call({
-          method: "awesome_pos_hw.api.get_hardware_details.hardware_url",
-          args: { api_name: api_name },
-          callback: function (r) {
-            if (r.message) {
-              resolve(r.message);
-            } else {
-              frappe.msgprint(
-                __("Hardware URL not configured for {0}", [api_name])
-              );
-              reject("URL not configured");
-            }
-          },
-          error: function (err) {
-            reject(err);
-          },
-        });
-      });
-    },
-    async custom_print(invoice_name) {
-      const url = await this.hardwareURL("Printer");
 
-      if (!url) {
-        frappe.msgprint(__("Printer URL not configured"));
-        return;
+    async hardwareURL(api_name) {
+      try {
+        const url = await frappeCall(
+          "awesome_pos_hw.api.get_hardware_details.hardware_url",
+          { api_name }
+        );
+        if (!url) {
+          frappe.msgprint(
+            __("Hardware URL not configured for {0}", [api_name])
+          );
+          throw new Error("URL not configured");
+        }
+        return url;
+      } catch (err) {
+        throw err;
       }
-      url &&
-        frappe.call({
-          method: "posawesome.posawesome.api.pos_sales_hm.generate_print_xml",
-          args: {
+    },
+
+    async custom_print(invoice_name) {
+      try {
+        const url = await this.hardwareURL("Printer");
+        if (!url) return;
+
+        const xmlPayload = await frappeCall(
+          "posawesome.posawesome.api.pos_sales_hm.generate_print_xml",
+          {
             doc_type: "Sales Invoice",
             sales_invoice_name: invoice_name,
             template_path:
               "posawesome/templates/print_formats/sales_invoice_template.xml",
-          },
-          callback: function (r) {
-            const xmlPayload = r.message;
+          }
+        );
 
-            fetch(url, {
-              method: "POST",
-              headers: {
-                "Content-Type": "application/xml;charset-utf-8",
-              },
-              body: xmlPayload,
-              mode: "cors",
-            })
-              .then((res) => {
-                if (res.ok) {
-                  frappe.show_alert({
-                    message: "Printing...",
-                    indicator: "green", // or 'green', 'red', etc.
-                  });
-                } else {
-                  frappe.show_alert({
-                    message: "Failed to print",
-                    indicator: "red", // or 'green', 'red', etc.
-                  });
-                }
-              })
-              .catch((err) => {
-                frappe.msgprint("Error: " + err);
-              });
-          },
+        const res = await fetch(url, {
+          method: "POST",
+          headers: { "Content-Type": "application/xml;charset=utf-8" },
+          body: xmlPayload,
+          mode: "cors",
         });
+
+        frappe.show_alert({
+          message: res.ok ? "Printing..." : "Failed to print",
+          indicator: res.ok ? "green" : "red",
+        });
+      } catch (err) {
+        frappe.msgprint("Error: " + (err.message || err));
+      }
     },
   },
 };
