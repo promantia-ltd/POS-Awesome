e8cf9439c13d398772489c123a6dc22aa490ac93
MadhusudhanMS <madhusudhan.m@promantia.com>
2025-08-25 11:08:43 +0530

    26295 -Return Amount Updating Incorrectly, not working in verison 14 and 15

--- posawesome/posawesome/api/posapp.py
+++ posawesome/posawesome/api/posapp.py
@@ -678,14 +678,15 @@ def submit_invoice(invoice, data):
             and not is_cashback
         ):
             
-            original_invoice = frappe.get_doc("Sales Invoice", invoice_doc.return_against)
-            custom_delivery_charge = flt(original_invoice.get("custom_delivery_charge_rate") or 0)
-            return_grand_total = flt(invoice_doc.grand_total) - custom_delivery_charge
-            original_grand_total = flt(original_invoice.grand_total)
-            new_outstanding = -(return_grand_total + original_grand_total)
-            original_invoice.db_set("outstanding_amount", new_outstanding)
-            original_invoice.set_status()
-
+           original_invoice = frappe.get_doc("Sales Invoice", invoice_doc.return_against)
+           custom_delivery_charge = flt(original_invoice.get("custom_delivery_charge_rate") or 0)
+           return_grand_total = flt(invoice_doc.grand_total) - custom_delivery_charge
+           original_grand_total = flt(original_invoice.outstanding_amount) 
+           new_outstanding = original_grand_total + return_grand_total  
+           original_invoice.db_set("outstanding_amount", new_outstanding)
+           original_invoice.set_status()
+           invoice_doc.db_set("outstanding_amount", 0)
+           invoice_doc.set_status()
 
         redeeming_customer_credit(
             invoice_doc, data, is_payment_entry, total_cash, cash_account, invoice_doc.payments
