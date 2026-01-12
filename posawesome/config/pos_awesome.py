bd73949c087e3b6a466cf4249aad7419809f3c6e
Gagan-promantia <gagan.gr@promantia.com>
2026-01-12 10:36:09 +0530

    chore: update PosAwesome app name in config files

--- posawesome/config/pos_awesome.py
+++ posawesome/config/pos_awesome.py
@@ -4,10 +4,10 @@ from frappe import _
 def get_data():
 	return [
 		{
-			"label": _("POS Awesome"),
+			"label": _("POSpire"),
 			"items": [
 				 {
-				   "description": "POS Awesome", 
+				   "description": "POSpire", 
 				   "name": "posapp", 
 				   "label": "POSAPP", 					
 				   "type": "page"
